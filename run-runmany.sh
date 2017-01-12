308969bc75178a27126b5c1c1705dc335cc512c9
Brent Barker <me@brentwbarker.net>
2017-01-12 17:38:52 -0500

    Add run-runmany.sh script.

---
+++ run-runmany.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pbuu/runmany.sh -b '5' -e '0.4' -r 300 -s '197au197au' -w '/projects/hira/barker/pbuu-working' -i '0.6' -c 'LCSM' -o 'true' -p 'true false' -m 'dep' -n 'soft' -t 'tsang'
+
